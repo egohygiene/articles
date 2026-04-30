45ca278c9dea673fd11d38df17af499a95248781
Alan Szmyt <szmyty@gmail.com>
2026-04-30 10:50:55 -0400

    Checkpoint

--- mindlint/rules/emoji.py
+++ mindlint/rules/emoji.py
@@ -1,15 +1,20 @@
+import emoji
+
 from mindlint.rules.base import Rule, Issue
 from mindlint.models.article import Article
 
 
+def has_emoji_prefix(text: str) -> bool:
+    emojis = emoji.emoji_list(text)
+    return bool(emojis and emojis[0]["match_start"] == 0)
+
+
 class EmojiTitleRule(Rule):
-    def check(self: "EmojiTitleRule", article: Article) -> list[Issue]:
+    def check(self, article: Article) -> list[Issue]:
         if not article.title:
             return [Issue("Missing title", "error")]
 
-        first_char: str = article.title[0]
-
-        if ord(first_char) < 10000:
+        if not has_emoji_prefix(article.title):
             return [Issue("Missing emoji prefix")]
 
         return []
